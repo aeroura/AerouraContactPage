798948cd93c294edab93c2f0b8a58cc04c04a19a
Joab <joabbastidas@gmail.com>
2021-01-26 16:02:10 -0500

    Moved Image locations

--- src/LandingPageImage.js
+++ src/LandingPageImage.js
@@ -3,9 +3,11 @@ import "./LandingPageImage.css";
 
 export default class LandingPageImage extends Component {
     render() {
+        
         return (
-            <div>
-                
+            <div className="landingPage" style={{ backgroundImage: "url(/BrooklynBridge.jpg)" }}>
+                <h1>Aeroura Travel</h1>
+                <h3>Experience The World</h3>
             </div>
         )
     }
