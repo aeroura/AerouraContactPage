e6d06e8c5d6295f24a077fc8e85995acef743853
Joab <joabbastidas@gmail.com>
2021-01-29 17:51:42 -0500

    Added code changes

--- src/SpecializeBoxes.js
+++ src/SpecializeBoxes.js
@@ -4,8 +4,34 @@ import "./SpecializeBoxes.css";
 export default class SpecializeBoxes extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="SpecializeBoxes">
+                <h3>We Specialize In...</h3>
+                <div className="row">
+                    <div className="col">
+                        <h4>Lesiure Travel</h4>
+                        <div className="box">
+                            <image></image>
+                            <p>Relax and Enjoy.</p>
+                            <p>Relax and Enjoy.</p>
+                        </div>
+                    </div>
+                    <div className="col">
+                    <h4>Planning Trips</h4>
+                        <div className="box">
+                            <image src="BrooklynBridge.jpg"></image>
+                            <p>Relax and Enjoy.</p>
+                            <p>Relax and Enjoy.</p>
+                        </div>
+                    </div>
+                    <div className="col">
+                    <h4>New York City</h4>
+                        <div className="box">
+                            <image></image>
+                            <p>Relax and Enjoy.</p>
+                            <p>Relax and Enjoy.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/TravelBanner.js
+++ src/TravelBanner.js
@@ -4,8 +4,8 @@ import "./TravelBanner.css";
 export default class TravelBanner extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="travelBanner">
+                <h1 className="travelBannerText">NYC TRAVEL</h1>
             </div>
         )
     }
