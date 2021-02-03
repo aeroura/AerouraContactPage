4bcbee6fb56886fa0ea4cfaccd5311fe37bd6f74
Joab <joabbastidas@gmail.com>
2021-02-03 15:16:11 -0500

    almost done with website

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import "./Footer.css";
+import {FaTwitter, FaFacebookSquare, FaLinkedin, FaInstagram} from 'react-icons/fa';
 
 export default class Footer extends Component {
     render() {
         return (
     <div className="main-footer">
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
-            <div className="col">Aeroura Travel
-              &copy;{new Date().getFullYear()} 
-              Privacy Policy Terms & Conditions
+            <div className="col-md-5">
+              <ul className="footerList">
+                <li>Aeroura Travel</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+              </ul>
+               {/*&copy;{new Date().getFullYear()}*/}
             </div>
-            <div className="col">
-              <h6>Facebook</h6>
-              <h6>Twitter</h6>
-              <h6>Gmail</h6>
-              <h6>Instagram</h6>
-              <h6>Linkedin</h6>
+            <div className="col-md-5"></div>
+            <div className="col-md-2">
+              <div className="icons">
+                  <FaFacebookSquare
+                    className="footer-icons"
+                    />
+                  <FaTwitter
+                  className="footer-icons"
+                  />
+                  <FaInstagram
+                  className="footer-icons"
+                  />
+                  <FaLinkedin
+                  className="footer-icons"
+                  />    
+              </div>
             </div>
           </div>
         </div>
--- src/SpecializeBoxes.js
+++ src/SpecializeBoxes.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import "./SpecializeBoxes.css";
+import BrooklynBridge from './Images/BrooklynBridge.jpg';
+import Macys from './Images/Macy\'s.jpg';
+import BroadwayStreet from './Images/BroadwayStreet.jpg';
 
 export default class SpecializeBoxes extends Component {
     render() {
         return (
             <div className="SpecializeBoxes">
-                <h3>We Specialize In...</h3>
+                <h3 className="headingText">We Specialize In...</h3>
                 <div className="row">
-                    <div className="col">
-                        <h4>Lesiure Travel</h4>
-                        <div className="box">
-                            <image></image>
-                            <p>Relax and Enjoy.</p>
-                            <p>Relax and Enjoy.</p>
+                    <div className="col-md-4">
+                       <h4 className="headingText">Local Travel</h4> 
+                       <div className="box">
+                        <img src={BrooklynBridge} alt="BrooklynBridge" className="img-fluid"></img>
+                            <div className="descriptionText">
+                                <p>Relax and Enjoy. There's not much else to it and we're just fine with that.</p>
+                                <p>A trip for the entire family or a solo adventure is just around the corner.</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="col">
-                    <h4>Planning Trips</h4>
+                    <div className="col-md-4">
+                    <h4 className="headingText">Planning Trips</h4>
                         <div className="box">
-                            <image src="BrooklynBridge.jpg"></image>
-                            <p>Relax and Enjoy.</p>
-                            <p>Relax and Enjoy.</p>
+                        <img src={Macys} alt="Macys" className="img-fluid"></img>
+                            <div className="descriptionText">
+                                <p>There's magic in planning and we can't get enough of it.</p>
+                                <p>We enjoy planning your vacation, weekend Getaways, and family retreats.</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="col">
-                    <h4>New York City</h4>
+                    <div className="col-md-4">
+                    <h4 className="headingText">New York City</h4>
                         <div className="box">
-                            <image></image>
-                            <p>Relax and Enjoy.</p>
-                            <p>Relax and Enjoy.</p>
+                        <img src={BroadwayStreet} alt="BroadwayStreet" className="img-fluid"></img>
+                            <div className="descriptionText">
+                                <p>The best and brightest come to New York. There's no place like it so reach out to us to get the ins-and-outs of our booming city.</p>
+                                <p>We love it and we know you will too.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
