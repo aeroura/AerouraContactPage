bfac2d7fffbf90b258a981b2974aaa2bba1644be
Joab <joabbastidas@gmail.com>
2021-02-25 14:22:53 -0500

    Finished the dropdown section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useEffect, useRef, useState} from 'react';
 import './App.css';
 import Footer from "./Footer.js";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,38 +8,57 @@ import Home from "./Home.js";
 import About from "./About.js";
 /*import Community from "./Community.js";*/
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 
 function App() {
   /* useState for hamburger menu */
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
-/* useState for dropdown menu */
+  /* useState for dropdown menu */
   const [showDropDown, setShowDropDown] = useState(false);
   const handleShowDropDown = () => setShowDropDown(!showDropDown);
+
+  /* Used to hide dropdown on outside click */
+    const myRef = useRef();
+
+    const handleClickOutside = e => {
+        if (!myRef.current.contains(e.target)) {
+          setShowDropDown(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    });
+
   return (
 
   <BrowserRouter>
     <div className="App">
 {/* Header with links Navbar Bootstrap */}
 <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navHeader">
+  {/* Hamburger menu button */}
 <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" 
     aria-controls="navbar" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" 
     onClick={handleNavCollapse}>
     <span className="navbar-toggler-icon"></span>
   </button>
+  {/* Hamburger menu section */}
   <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarToggle">
-    <span className="homeLink"><Link to="/">AEROURA TRAVEL</Link></span>
+    <span className="homeLink"><NavLink exact to="/" activeClassName="active" style={{textDecoration: 'none'}}
+    >AEROURA TRAVEL</NavLink></span>
       <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li className="nav-item"><Link to="/about">About</Link></li>
+      <li className="nav-item"><Link to="/about" style={{textDecoration: 'none'}}>About</Link></li>
       {/*<li className="nav-item"><Link to="/community">Community</Link></li>*/}
       
     </ul>
-      <div className="dropdown">
+     {/* Dropdown section */}
+   <div className="dropdown">
       <button className="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" 
-       onClick={handleShowDropDown} aria-expanded="false">
-        <FontAwesomeIcon icon={faGlobe} size="2x" style={{ color: 'rgb(19, 143, 137)' }} />
+       onClick={handleShowDropDown} aria-expanded="false" ref={myRef}>
+        <FontAwesomeIcon icon={faGlobe} size="2x" style={{ color: 'rgb(19, 143, 137)' }} id="icon" />
       </button>
       {showDropDown ?
       <div className="dropdown" aria-labelledby="dropdownMenuButton" id="dropDownMenu">
@@ -47,7 +66,7 @@ function App() {
         <a className="dropdown-item" href="google.com">Spanish</a>
       </div> 
       : null}
-    </div>
+    </div>  
   </div>
 </nav>
 
@@ -55,10 +74,12 @@ function App() {
       <div className="content-wrap">
         {/* Contains website data in multiple routes */}
           <Switch>
-              <Route exact path="/" component={Home} render={()=> (
-                <Redirect to="home"/>
-              )} />
-              <Route exact path="/about" component={About} />
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/about">
+              <About/>
+            </Route>
               {/* <Route path="/community" component={Community} /> */}
              {/* This gives an error  <Route component={Error} /> */}
           </Switch>
@@ -76,20 +97,40 @@ export default App;
 
 /* 
 
-<DropdownButton id="dropdown-item-button" title="Dropdown button">
-<FontAwesomeIcon icon={faGlobe} size="2x" style={{ color: 'rgb(19, 143, 137)' }} />
-  <Dropdown.Item as="button">English</Dropdown.Item>
-  <Dropdown.Item as="button">Spanish</Dropdown.Item>
-</DropdownButton>
+----Replace if above doesnt work------
+<Route exact path="/" component={Home}/>
+<Route exact path="/about" component={About} />
+
+--------Extra Info for Navlink-------
+<NavLink className="navbar-item" activeClassName="is-active" to="/">
+  Home
+</NavLink>
 
 
-<div className="dropdown">
-      <button className="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <FontAwesomeIcon icon={faGlobe} size="2x" style={{ color: 'rgb(19, 143, 137)' }} />
-      </button>
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" id="menu">
-        <a className="dropdown-item" href="google.com">English</a>
-        <a className="dropdown-item" href="google.com">Spanish</a>
-      </div>
+
+
+const SampleComponent = () => {
+    const [clickedOutside, setClickedOutside] = useState(false);
+    const myRef = useRef();
+
+    const handleClickOutside = e => {
+        if (!myRef.current.contains(e.target)) {
+            setClickedOutside(true);
+        }
+    };
+
+    const handleClickInside = () => setClickedOutside(false);
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    });
+
+    return (
+        <button ref={myRef} onClick={handleClickInside}>
+            {clickedOutside ? 'Bye!' : 'Hello!'}
+        </button>
+    );
+};
 
 */
