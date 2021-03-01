5c3bd7320ce147887af40d324b3e44842c09148a
Joab <joabbastidas@gmail.com>
2021-03-01 16:31:32 -0500

    Worked on home page responsiveness

--- src/Home.js
+++ src/Home.js
@@ -22,51 +22,39 @@ export default function Home() {
               <p className="subTitle">{t("EXPERIENCE THE WORLD")}</p>
           </div>
       </div>
-      <div className="container-fluid">
+      <div className="descriptionBoxes">
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-4 col-sm-6 col-xs-12">
                         <h4>Places to See - NYC</h4>
                         <div className="PlacesToSee">
-                            <img src={BrookfieldPlace} alt="BrookfieldPlace" width={604} height={270} mode='fit'></img>
+                            <img src={BrookfieldPlace} alt="BrookfieldPlace" className="slideShowImage"></img>
                             <h6>Brookfield Place - </h6>
                         </div>
                     </div>
-                    <div className="col-md-4">
-                        <div className="TravelCounselor">
-                            <div className="row">
-                                <div className="col-md-3">
-                                    <img src={JehieliProfessional} alt="Josue" width={200} height={300}></img>
-                                </div>
-                                <div className="col-md-9">
-                                    <div className="Name">
-                                        <h3>Josue Jehieli Bastidas</h3>
-                                        <h5>Travel Counselor</h5>
-                                    </div>
-                                    <div className="ContactInfo">
-                                        <p><FontAwesomeIcon icon={faPhoneAlt} style={{ color: 'rgb(19, 143, 137)' }} /> (347)-619-2214</p>
-                                        <p><FontAwesomeIcon icon={faEnvelope} style={{ color: 'rgb(19, 143, 137)' }} /> Josue@aeroura.com</p>
-                                        <button type="submit" className="btn btn-primary">Schedule an Appointment</button>
-                                    </div>
-                                </div>
+                    <div className="col-md-4 col-sm-6 col-xs-12" id="TravelCounselor">
+                            <img src={JehieliProfessional} alt="Josue" className="josueImage"></img>
+                        <div className="josuePortraitInfo">
+                            <div className="Name">
+                                <h3>Josue Jehieli Bastidas</h3>
+                                <h5>Travel Counselor</h5>
+                            </div>
+                            <div className="ContactInfo">
+                                <p><FontAwesomeIcon icon={faPhoneAlt} style={{ color: 'rgb(19, 143, 137)' }} /> (347)-619-2214</p>
+                                <p><FontAwesomeIcon icon={faEnvelope} style={{ color: 'rgb(19, 143, 137)' }} /> Josue@aeroura.com</p>
+                                <button type="submit" className="btn btn-primary">Schedule an Appointment</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4">
-                        <div className="VirtualHours">
-                            <div className="row">
-                                <div className="col-md-7">
-                                    <img src={JehieliMountains} alt="JehieliMountains" width={350} height={300} mode='fit'></img>
-                                </div>
-                                <div className="col-md-5">
-                                    <div className="Title">
-                                        <h4><FontAwesomeIcon icon={faClock} style={{ color: 'rgb(19, 143, 137)' }} /> Virtual Hours</h4>
-                                        <h6>Monday, Wednesday, Friday</h6>
-                                        <h6>11am - 2pm (EST)</h6>
-                                    </div>
-                                    <div className="joinButton">
-                                        <button type="submit" className="btn btn-primary">Join Here</button>
-                                    </div>
-                                </div>
+                    <div className="col-md-4 col-sm-6 col-xs-12" id="VirtualHours">
+                        <img src={JehieliMountains} alt="JehieliMountains" className="josueMountainsImage"></img>
+                        <div className="virtualHoursInfo">
+                            <div className="Title">
+                                <h4><FontAwesomeIcon icon={faClock} style={{ color: 'rgb(19, 143, 137)' }} /> Virtual Hours</h4>
+                                <h6>Monday, Wednesday, Friday</h6>
+                                <h6>11am - 2pm (EST)</h6>
+                            </div>
+                            <div className="joinButton">
+                                <button type="submit" className="btn btn-primary">Join Here</button>
                             </div>
                         </div>
                     </div>
