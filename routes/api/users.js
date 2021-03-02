15b54a14c1c031490654e61556c8a04287fefc34
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2021-03-01 19:56:00 -0500

    Register A User
    Register logic works. Still need to work on the end result logic.

---
+++ routes/api/users.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+
+// @route POST api/users
+// @desc Register user
+// @access Public
+
+router.post(
+    '/',
+    [
+        check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 })        
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        
+        // Check/See if user exists
+
+        // Get users gravatar
+
+        // Encrypt password
+
+        // Return jsonwebtoken
+
+        res.send('User Route');
+    }
+);
+
+module.exports = router;
