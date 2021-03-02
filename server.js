595d6bce6586c5cb55fd4afd150a294cc928db3f
Alazy99 <57822868+Alazy99@users.noreply.github.com>
2021-03-01 19:57:52 -0500

    Bring the routes/database/middleware
    Bring all the files to the main app file

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Bring the Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
